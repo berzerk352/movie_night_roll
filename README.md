aea57a7c4aeb8a8670dff884dfae2d966315e870
berzerk352 <tyler.johnson.341@gmail.com>
2025-01-07 19:59:12 -0500

    add new logic to store previously rolled submitters and automatically reset once everyone  has been rolled through

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Random Movie Roll Script
 This project is dependent on a Google OAuth token. When you run the script, a browser window will open asking if you are OK with giving permission to the script. The script then has the ability to read any Google Docs/Sheets that your account does. 
 
-- `random_movie.ini` specifies which tab of the spreadsheet to roll through and stores the last user whose movie was selected.
+- `random_movie.ini` specifies which tab of the spreadsheet to roll through and stores the people who have had movies select so far this season. Rename `random_movie.ini.example` to `random_movie.ini` and customize as necessary. The initial user in the `season_roster` field is the column header for the user in the movie spreadsheet. 
 - `credentials.json` stores the OAuth secret used to authenticate. This is not checked into git and will need to be manually created 
 - `token.json` temporarily stores the credential to access Google docs. If the token has expired you'll have to manually delete this file and re-run the script
 
--- random_movie.py
+++ random_movie.py
@@ -9,18 +9,23 @@ def main(participants):
     config = configparser.ConfigParser()
     config.read('random_movie.ini')
 
-    previous_selectee = config['random_movie']['previous_selectee']
+    season_roster = config['random_movie']['season_roster'].split(',')
+    print(f"Already selected this season: {season_roster}")
     spreadsheet_range = f'{config["random_movie"]["spreadsheet_tab"]}!A:B'
     results = api_call(range=spreadsheet_range)
 
     if participants == "ALL":
         participants = reduce(lambda accum, val: 
-                              accum + [val[1].strip()] if val[1].strip() not in accum and val[1].strip() != "Submitter"
+                              accum + [val[1].strip()] if val[1].strip() not in accum and val[1].strip() not in season_roster
                               else accum, results, [])
 
-    print(participants)
-    participants = list(filter(lambda x: x.upper() != previous_selectee.upper(), participants ))
-
+    participants = list(participants)
+    if participants == []:
+        config['random_movie']['season_roster'] = 'Submitter'
+        participants = reduce(lambda accum, val: 
+                              accum + [val[1].strip()] if val[1].strip() not in accum
+                              else accum, results, [])
+    print(f"Eligible for tonight's roll: {participants}")
     random_user = random.choice(participants)
     print(f"This week's movie was nominated by {random_user}")
     
@@ -29,7 +34,7 @@ def main(participants):
     
     print(f"Your randomly selected movie is {random.choice(results)[0]}")
 
-    config['random_movie']['previous_selectee'] = random_user
+    config['random_movie']['season_roster'] = config['random_movie']['season_roster'] + f",{random_user}"
     with open('random_movie.ini','w') as configfile:
         config.write(configfile)
     
