d3b138bbd72d1a1efdf1157af06fc6cfe4bbc305
berzerk352 <tyler.johnson.341@gmail.com>
2025-04-21 15:26:01 -0400

    convert to using service account over 2fa on every run

--- sheets_api.py
+++ sheets_api.py
@@ -1,30 +1,29 @@
 import os.path
-
+import pickle
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
 
 def api_call(
     spreadsheet_id="1AI2EqC73Z87U1Y47Fl068xvQnQXsZ85Yll3G7UKa1ps", range="General!A:B"
 ):
     creds = None
-
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+            creds = service_account.Credentials.from_service_account_file(
+                "credentials.json"
+            )
+            creds = creds.with_scopes(["https://www.googleapis.com/auth/spreadsheets"])
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
     try:
         service = build("sheets", "v4", credentials=creds)
