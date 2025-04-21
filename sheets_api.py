792db1e2751635d008045ff9783662804b820d11
berzerk352 <tyler.johnson.341@gmail.com>
2025-04-21 15:30:26 -0400

    remove unneeded includes

--- sheets_api.py
+++ sheets_api.py
@@ -1,9 +1,7 @@
 import os.path
 import pickle
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
